f3796534598cdea94cd6ea87cd72f652ac19d0cc
Helder <he7d3r@gmail.com>
2012-03-13 19:47:35 +0000

    Add a template to create new personal scripts
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/Modelo.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/PAGENAME.js]] ([[File:User:Helder.wiki/Tools/PAGENAME.js]])
+ */
+/*global $, mw */
+/*jslint white: true */
+$(function () {
+'use strict';
+
+
+
+});
