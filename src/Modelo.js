6b6816704a86a5a8f5466d713bb7eb089b9a752a
Helder <he7d3r@gmail.com>
2012-03-17 17:18:27 +0000

    Change the order of mediaWiki and jQuery

--- src/Modelo.js
+++ src/Modelo.js
@@ -4,7 +4,7 @@
  * @tracking: [[Special:GlobalUsage/User:{{subst:PAGENAME}}]] ([[File:User:{{subst:PAGENAME}}]])
  */
 /*jslint browser: true, white: true*/
-/*global mediaWiki, jQuery*/
+/*global jQuery, mediaWiki */
 ( function ( $, mw /* , undefined */ ) {
 'use strict';
 
