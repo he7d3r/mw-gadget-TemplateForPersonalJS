4867a2c68790f4472cb1baf09f6a4f4ace2d4082
Helder <he7d3r@gmail.com>
2014-05-01 20:26:07 +0000

    Add some linebreaks

--- src/Modelo.js
+++ src/Modelo.js
@@ -3,7 +3,9 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Modelo.js]] ([[File:User:Helder.wiki/Tools/Modelo.js]])
  */
-/*jshint browser:true, camelcase:true, curly:true, eqeqeq:true, immed:true, latedef:true, newcap:true, noarg:true, noempty:true, nonew:true, quotmark:true, undef:true, unused:true, strict:true, trailing:true, maxlen:120, evil:true, onevar:true */
+/*jshint browser:true, camelcase:true, curly:true, eqeqeq:true, immed:true, latedef:true,
+newcap:true, noarg:true, noempty:true, nonew:true, quotmark:true, undef:true, unused:true,
+strict:true, trailing:true, maxlen:120, evil:true, onevar:true */
 /*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
